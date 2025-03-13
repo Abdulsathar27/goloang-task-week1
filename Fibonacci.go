46d0580a94d68e5e40384e0653c0c49b2c58a1e8
abdul sathar <abdulsatharnechithodi@gmail.com>
2025-03-13 14:46:12 +0530

    second

--- Fibonacci.go
+++ Fibonacci.go
@@ -1,25 +1,27 @@
 package main
+
 import "fmt"
 
-func printFibonacciSeries(num int){
+func printFibonacciSeries(num int) {
 	a := 0
 	b := 1
-	c := b
-	fmt.Printf("Fibonacci Series is :%d",a,b)
-	for true{
-		c = b
-		b = a + b 
-		if b >=num {
-			fmt.Println()
+
+	fmt.Printf("Fibonacci Series up to %d: %d %d ", num, a, b)
+
+	for {
+		c := a + b
+		if c >= num {
 			break
 		}
-		a = c
-		fmt.Printf("%d",b)
+		fmt.Printf("%d ", c)
+		a = b
+		b = c
 	}
-
+	fmt.Println() 
 }
-func main (){
-  printFibonacciSeries(10)
-  printFibonacciSeries(16)
-  printFibonacciSeries(100)
+
+func main() {
+	printFibonacciSeries(10)
+	printFibonacciSeries(16)
+	printFibonacciSeries(100)
 }
